ccb0c67d679e2266a74d4cc25b812297c20b1120
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-01 14:27:54 -0700

    Working item selector

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,18 +13,41 @@ function displayProducts() {
         function (err, res) {
             if (err) throw err;
             console.table(res);
-            userInput();
+            userInput(res);
             connection.end();
         });
 };
 
-function userInput() {
+function userInput(data) {
     inquirer.prompt([
         {
-            
+            message: "What is the item_id of the item you wish to purchase?",
+            name: "idNumber"
+        },
+        {
+            message: "How many of this item would you like to purchase?",
+            name: "quantity"
         }
-    ])
+    ]).then(answer => {
+        var item = data[answer.idNumber - 1];
+        if (item.stock_quantity < answer.quantity || answer.quantity < 0) {
+            console.log("Insufficient quantity!");
+        } else {
+            var totalPrice = item.price * answer.quantity;
+            console.log(`You have purchased ${answer.quantity} ${item.product_name} for $${totalPrice}!`);
+        }
+    })
 };
+
+function updateDB() {
+    connection.query("update products set ? where ?",
+    {
+        
+    },
+    {
+
+    })
+}
 
 connection.connect(err => {
     if (err) throw err;
