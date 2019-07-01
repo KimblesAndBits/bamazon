49696ce9a6658b8ee2014279a86701be5f19a5c2
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-01 15:08:38 -0700

    working manager view display all and low inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,3 +8,56 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+function displayProducts() {
+    connection.query("select * from products",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            managerView();
+        });
+};
+
+function lowInventory() {
+    connection.query("select * from products where stock_quantity < 5",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            managerView();
+        });
+};
+
+function managerView() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do, manager?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
+            name: "manChoice"
+        }
+    ]).then(answer => {
+        switch (answer.manChoice) {
+            case "View Products for Sale":
+                displayProducts();
+                break;
+            case "View Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                console.log("Add to inventory");
+                managerView();
+                break;
+            case "Add New Product":
+                console.log("Add new product.");
+                managerView();
+                break;
+            case "Exit":
+                console.log("Thanks for using bamazon Manager View!");
+                connection.end();
+        };
+    });
+};
+
+connection.connect(err => {
+    if (err) throw err;
+    managerView();
+});
