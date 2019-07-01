3f88baa9f75ac013d346b8e419c2f0ae7a8233d9
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-01 14:13:01 -0700

    working products display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,32 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Zzros534",
+    database: "bamazon"
+});
+
+function displayProducts() {
+    connection.query("select * from products",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            userInput();
+            connection.end();
+        });
+};
+
+function userInput() {
+    inquirer.prompt([
+        {
+            
+        }
+    ])
+};
+
+connection.connect(err => {
+    if (err) throw err;
+    displayProducts();
+});
